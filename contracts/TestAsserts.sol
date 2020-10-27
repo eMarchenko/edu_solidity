b7dded293288555e25d71908ab2ed1e0282118cb
Evgeny Marchenko <marchenko@smartdec.ru>
2020-10-27 22:08:22 +0300

    add obviously failing functions

--- contracts/TestAsserts.sol
+++ contracts/TestAsserts.sol
@@ -7,8 +7,15 @@ contract TestAsserts is Test(2) {
     function assertNotFive(uint y) external view {
         assert(value != y);
     } 
+    function assertFails() external pure {
+        assert(false);
+    } 
+
 
     function echidnaNotFive() external view returns (bool) {
         return value != 5;
+    }
+    function echidnaShouldFail() external pure returns (bool) {
+        return false;
     } 
 }
